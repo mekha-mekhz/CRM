8394bffccbcc3385b72c92e9e62d20f754ce83e5
Mekha <mohanananmekha04@gmail.com>
2025-10-25 12:11:34 +0530

    routes cleared

--- routes/caseroute.js
+++ routes/caseroute.js
@@ -1,8 +1,8 @@
 const express=require('express')
 const router=express.Router()
 const casecontroller=require('../controllers/casecontroll')
-router.post("/cases", casecontroller.createcase);      
-router.get("/cases", casecontroller.getcase);          
-router.patch("/cases/:id", casecontroller.updatecase);
-router.delete("/cases/:id", casecontroller.deletecase);
+router.post("/", casecontroller.createcase);      
+router.get("/", casecontroller.getcase);          
+router.patch("/:id", casecontroller.updatecase);
+router.delete("/:id", casecontroller.deletecase);
 module.exports=router
